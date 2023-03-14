9b5d061e5bd30ff5ff5efaffe40dbc501e482466
sdwiyanti <125328594+sdwiyanti@users.noreply.github.com>
2023-03-14 07:48:18 +0700

    Update main.py

--- challenge_data/challenge_1/main.py
+++ challenge_data/challenge_1/main.py
@@ -1,83 +1,43 @@
-import random
-
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error, explained_variance_score, r2_score
 
 def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwargs):
-    print("Starting Evaluation.....")
-    print("Submission related metadata:")
-    """
-    Evaluates the submission for a particular challenge phase adn returns score
-    Arguments:
+    # Load the ground truth labels and predictions into pandas dataframes
+    ground_truth = pd.read_csv(test_annotation_file)
+    prediction = pd.read_csv(user_submission_file)
 
-        `test_annotations_file`: Path to test_annotation_file on the server
-        `user_submission_file`: Path to file submitted by the user
-        `phase_codename`: Phase to which submission is made
+    # Merge the two dataframes on the "id" and "word" columns
+    merged_df = pd.merge(ground_truth, prediction, on=['Item_Identifier', 'Outlet_Identifier'])
 
-        `**kwargs`: keyword arguments that contains additional submission
-        metadata that challenge hosts can use to send slack notification.
-        You can access the submission metadata
-        with kwargs['submission_metadata']
+    # define independent dan dependent variables
+    # X = merged_df['independent_var1']
+    y_true = merged_df['Item_Outlet_Sales22']
+    y_pred = merged_df['Item_Outlet_Sales']
 
-        Example: A sample submission metadata can be accessed like this:
-        >>> print(kwargs['submission_metadata'])
-        {
-            "status": u"running",
-            "when_made_public": None,
-            "participant_team": 5,
-            "input_file": "https://abc.xyz/path/to/submission/file.json",
-            "execution_time": u"123",
-            "publication_url": u"ABC",
-            "challenge_phase": 1,
-            "created_by": u"ABC",
-            "stdout_file": "https://abc.xyz/path/to/stdout/file.json",
-            "method_name": u"Test",
-            "stderr_file": "https://abc.xyz/path/to/stderr/file.json",
-            "participant_team_name": u"Test Team",
-            "project_url": u"http://foo.bar",
-            "method_description": u"ABC",
-            "is_public": False,
-            "submission_result_file": "https://abc.xyz/path/result/file.json",
-            "id": 123,
-            "submitted_at": u"2017-03-20T19:22:03.880652Z",
-        }
-    """
-    print(kwargs["submission_metadata"])
+    mae = mean_absolute_error(y_true, y_pred)
+    mse = mean_squared_error(y_true, y_pred)
+    rmse = mean_squared_error(y_true, y_pred, squared=False)
+    r2 = r2_score(y_true, y_pred)
+    evs = explained_variance_score(y_true, y_pred)
+    mape = np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
     output = {}
-    if phase_codename == "dev":
-        print("Evaluating for Dev Phase")
+    if phase_codename == "test":
+        print("Evaluating for Test Phase")
         output["result"] = [
             {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
+                "test_split": {
+                    'Mean Absolute Error (MAE)': mae,
+                    'Mean Squared Error (MSE)' : mse,
+                    'Root Mean Squared Error (RMSE)' : rmse,
+                    'R-squared (R2)' : r2,
+                    'Explained Variance Score (EVS)' : evs,
+                    'Mean Absolute Percentage Error (MAPE)' : mape,
                 }
             }
         ]
         # To display the results in the result file
-        output["submission_result"] = output["result"][0]["train_split"]
-        print("Completed evaluation for Dev Phase")
-    elif phase_codename == "test":
-        print("Evaluating for Test Phase")
-        output["result"] = [
-            {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            },
-            {
-                "test_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            },
-        ]
-        # To display the results in the result file
-        output["submission_result"] = output["result"][0]
+        output["submission_result"] = output["result"][0]["test_split"]
         print("Completed evaluation for Test Phase")
     return output
