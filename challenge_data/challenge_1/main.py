b8fa36f0536a687a79c6740917af58f7bf53694a
sdwiyanti <125328594+sdwiyanti@users.noreply.github.com>
2023-05-08 19:42:28 +0700

    Update main.py

--- challenge_data/challenge_1/main.py
+++ challenge_data/challenge_1/main.py
@@ -1,50 +1,43 @@
-import pandas as pd
-import numpy as np
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-import statsmodels.api as sm
-from scipy.stats import linregress
+ import pandas as pd
+ import numpy as np
+ from sklearn.metrics import mean_absolute_error, mean_squared_error, explained_variance_score, r2_score
 
-def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwargs):
-    # Load the ground truth labels and predictions into pandas dataframes
-    ground_truth = pd.read_csv(test_annotation_file)
-    prediction = pd.read_csv(user_submission_file)
+ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwargs):
+     # Load the ground truth labels and predictions into pandas dataframes
+     ground_truth = pd.read_csv(test_annotation_file)
+     prediction = pd.read_csv(user_submission_file)
 
-    # Merge the two dataframes on the "id" and "word" columns
-    merged_df = pd.merge(ground_truth, prediction, on=['Item_Identifier', 'Outlet_Identifier'])
+     # Merge the two dataframes on the "id" and "word" columns
+     merged_df = pd.merge(ground_truth, prediction, on=['Item_Identifier', 'Outlet_Identifier'])
 
-    # define independent dan dependent variables
-    # X = merged_df['independent_var1']
-    y_true = merged_df['Item_Outlet_Sales22']
-    y_pred = merged_df['Item_Outlet_Sales']
+     # define independent dan dependent variables
+     # X = merged_df['independent_var1']
+     y_true = merged_df['Item_Outlet_Sales22']
+     y_pred = merged_df['Item_Outlet_Sales']
 
-    mae = mean_absolute_error(y_true, y_pred)
-    mse = mean_squared_error(y_true, y_pred)
-    rmse = mean_squared_error(y_true, y_pred, squared=False)
+     mae = mean_absolute_error(y_true, y_pred)
+     mse = mean_squared_error(y_true, y_pred)
+     rmse = mean_squared_error(y_true, y_pred, squared=False)
+     r2 = r2_score(y_true, y_pred)
+     evs = explained_variance_score(y_true, y_pred)
+     wa = (mae * 0.2) + (mse * 0.3) + (rmse * 0.3) + (r2 * 0.1) + (evs * 0.1)
 
-    r2 = sm.OLS(y_true, y_pred).fit().rsquared
-    slope, intercept, r_value, p_value, std_err = linregress(y_true, y_pred)
-    evs = r_value ** 2
-    
-    #evs = explained_variance_score(y_true, y_pred)
-    # mape = np.mean(np.abs((y_true - y_pred) / y_true)) * 100
-    wa = (mae * 0.2) + (mse * 0.3) + (rmse * 0.3) + (r2 * 0.1) + (evs * 0.1)
-
-    output = {}
-    if phase_codename == "dev":
-        print("Evaluating for Test Phase")
-        output["result"] = [
-            {
-                "test_split": {
-                    'Mean-absolute-error': mae,
-                    'Mean-squared-error' : mse,
-                    'Root-mean-squared-error' : rmse,
-                    'R-squared' : r2,
-                    'Explained-variance-score' : evs,
-                    'Average' : wa,
-                }
-            }
-        ]
-        # To display the results in the result file
-        output["submission_result"] = output["result"][0]["test_split"]
-        print("Completed evaluation for Test Phase")
-    return output
+     output = {}
+     if phase_codename == "dev":
+         print("Evaluating for Test Phase")
+         output["result"] = [
+             {
+                 "test_split": {
+                     'Mean-absolute-error': mae,
+                     'Mean-squared-error' : mse,
+                     'Root-mean-squared-error' : rmse,
+                     'R-squared' : r2,
+                     'Explained-variance-score' : evs,
+                     'Average' : wa,
+                 }
+             }
+         ]
+         # To display the results in the result file
+         output["submission_result"] = output["result"][0]["test_split"]
+         print("Completed evaluation for Test Phase")
+     return output
