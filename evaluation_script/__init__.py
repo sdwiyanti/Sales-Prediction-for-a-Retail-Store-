05762b68180710e986cd944a5fbfdcc28797e53d
sdwiyanti <125328594+sdwiyanti@users.noreply.github.com>
2023-05-09 07:37:42 +0700

    Update __init__.py

--- evaluation_script/__init__.py
+++ evaluation_script/__init__.py
@@ -4,47 +4,6 @@ import sys
 from pathlib import Path
 
 def install(package):
-    Install a pip python package
-
-    Args:
-        package ([str]): statsmodels==0.14.0
-        package ([str]): scipy==1.10.1
-    
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-def install_local_package(folder_name):
-    Install a local python package
-
-    Args:
-        folder_name ([str]): name of the folder placed in evaluation_script/
-    
-    subprocess.check_output(
-    [
-        sys.executable,
-        "-m",
-        "pip",
-        "install",
-        os.path.join(str(Path(__file__).parent.absolute()) + folder_name),
-    ]
-)
-
-install("shapely==1.7.1")
-install("requests==2.25.1")
-
-install_local_package("package_folder_name")
-
-
-"""
-# Q. How to install custom python pip packages?
-
-# A. Uncomment the below code to install the custom python packages.
-
-import os
-import subprocess
-import sys
-from pathlib import Path
-
-def install(package):
     # Install a pip python package
 
     # Args:
@@ -69,11 +28,11 @@ def install_local_package(folder_name):
     ]
 )
 
+# Install packages from PyPI
 install("shapely==1.7.1")
 install("requests==2.25.1")
 
+# Install custom packages
+install("statsmodels==0.14.0")
+install("scipy==1.10.1")
 install_local_package("package_folder_name")
-
-"""
-
-from .main import evaluate
